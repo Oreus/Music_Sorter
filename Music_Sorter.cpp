96ab2f64e8304f9084902f9612ae3f2727ed8511
Oreus <Oreus14@outlook.com>
2025-11-18 01:24:19 -0600

    Updated Music_Sorter.cpp to autoload csv

--- Music_Sorter.cpp
+++ Music_Sorter.cpp
@@ -1,13 +1,13 @@
 // Oscar Lopez Cardoso
 // A01713355
-// 10/24/2025
+// 11/18/2025
 
 #include "Song_N_List.h"
 
 int main() {
-
-    UI lista;
-    lista.begin();
+    List* list = new List();
+    list->loadFromCSV("songs.csv");
+    UI ui(list);
+    ui.begin();
     return 0;
-    
 }
